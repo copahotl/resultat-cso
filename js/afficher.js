be66f467c8989ac4af227812bbd3fb1e574e365b
copahotl <65412088+copahotl@users.noreply.github.com>
2025-06-17 12:00:24 +0200

    Add files via upload

---
+++ js/afficher.js
@@ -0,0 +1,10 @@
+function afficher(csvURL, sel) {
+    $.get(csvURL, function(data) {
+        const [header, ...rows] = data.trim().split('\n').map(r => r.split(','));
+        $(sel).DataTable({
+            data: rows,
+            columns: header.map(h => ({ title: h })),
+            responsive: true, paging: false, searching: true, info: false
+        });
+    });
+}
